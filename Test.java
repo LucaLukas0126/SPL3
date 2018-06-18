f8438163192d29dd41b5d0cb9252d983dc23d68e
lucalukas0126 <171wita17@o365.htl-leoben.at>
2018-06-18 14:37:20 +0200

    luki

--- Test.java
+++ Test.java
@@ -3,7 +3,18 @@ public class Test {
 
 	public static void main(String[] args) {
 
+		// args[0] = "Hallo";
+		// System.out.println(args.length);
+
+		// if(args[5].equals("Leoben")) {
+		// System.out.println("leoben ist meine Stadt bruh");
+		// }
+
 		
+			
+			
+		
+
 		
 	}
 
---
+++ taschenrechner.java
@@ -0,0 +1,31 @@
+
+public class taschenrechner {
+
+	public static void main(String[] args) {
+
+		int zahl1 = Integer.parseInt(args[0]);
+		int zahl2 = Integer.parseInt(args[2]);
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+		if (args[1].equals("+")) {
+			int addieren = zahl1 + zahl2;
+			System.out.println("Ergebnis: " +  addieren);
+		}
+		if (args[1].equals("-")) {
+			int subtrahieren = zahl1 - zahl2;
+			System.out.println("Ergebnis: " +  subtrahieren);
+		}
+		if (args[1].equals("*")) {
+			int multiplizieren = zahl1 * zahl2;
+			System.out.println("Ergebnis: " +  multiplizieren);
+		}
+		if (args[1].equals("/")) {
+			int dividieren = zahl1 / zahl2;
+			System.out.println("Ergebnis: " +  dividieren);
+		}
+		
+	}
+
+}
